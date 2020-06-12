04054c9273bc6a9699c46a6a95a8f2d18c20d235
JessieCheng <hamy820326@gmail.com>
2020-06-12 14:44:25 +0800

    add error page handle route

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,9 @@ router.use('/auth', require('./modules/auths.js'))
 router.use('/restaurants', authenticator, require('./modules/restaurant.js'))
 router.use('/', authenticator, require('./modules/home.js'))
 
+// error page handle
+router.get('*', (req, res) => {
+  res.render('error')
+})
+
 module.exports = router
